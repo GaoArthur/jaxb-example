b22cd403098ff7c1ad8159e8178e3939c3abeaae
Gao Yasen <gao@mailbox.unideb.hu>
2020-04-17 19:58:58 +0200

    Add a lego management
    Lego's detail

---
+++ src/main/java/legoset/LegoSet.java
@@ -0,0 +1,42 @@
+package legoset;
+
+import lombok.Data;
+import movie.YearAdapter;
+
+import javax.xml.bind.annotation.*;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+import java.net.URL;
+import java.time.Year;
+import java.util.List;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+@Data
+public class LegoSet {
+
+    @XmlAttribute(name = "number")
+    private int number;
+
+    private String name;
+    private String theme;
+    private String subtheme;
+
+    @XmlJavaTypeAdapter(YearAdapter.class)
+    private Year year;
+
+    private int pieces;
+
+    @XmlElementWrapper(name = "tags")
+    @XmlElement(name = "tag")
+    private List<String> tag;
+
+    @XmlElementWrapper(name = "minifigs")
+    @XmlElement(name = "minifig")
+    private List<Minifig> minifigs;
+
+    @XmlElement(name = "weight")
+    private List<Weight> weight;
+
+    private URL url;
+
+}
